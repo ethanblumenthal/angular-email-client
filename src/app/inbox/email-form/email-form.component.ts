cb5067248c81c8869449e27412154c1b8f756129
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-04-12 17:55:54 -0500

    Created reusable form to place in modal.

---
+++ src/app/inbox/email-form/email-form.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Email } from '../email';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-email-form',
+  templateUrl: './email-form.component.html',
+  styleUrls: ['./email-form.component.css'],
+})
+export class EmailFormComponent implements OnInit {
+  emailForm: FormGroup;
+  @Input() email: Email;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const { subject, from, to, text } = this.email;
+
+    this.emailForm = new FormGroup({
+      to: new FormControl(to, [Validators.required, Validators.email]),
+      from: new FormControl({ value: from, disabled: true }),
+      subject: new FormControl(subject, [Validators.required]),
+      text: new FormControl(text, [Validators.required]),
+    });
+  }
+}
